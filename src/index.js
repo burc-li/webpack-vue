556375abba3ef756e7a1936a5d09d582c7ba6b46
BurCha <1477996221@qq.com>
2020-04-19 22:38:11 +0800

    test:pre-commit

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Vuex from 'vuex'
 // import ElementUI from 'element-ui'
-import { Button, tag } from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import { Button, tag } from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './app.vue'
 
 import './assets/styles/global.less'
