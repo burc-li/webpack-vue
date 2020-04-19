aae6b16a5c6f73920b1b1128a5492ce4474c90d7
BurCha <1477996221@qq.com>
2020-04-19 22:37:24 +0800

    test:pre-commit

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import Vuex from 'vuex'
 // import ElementUI from 'element-ui'
 import { Button, tag } from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import App from './app.vue';
+import App from './app.vue'
 
-import './assets/styles/global.less';
-import createRouter from './config/router';
-import createStore from './store/store';
+import './assets/styles/global.less'
+import createRouter from './config/router'
+import createStore from './store/store'
 
 Vue.use(VueRouter)
 Vue.use(Vuex)
